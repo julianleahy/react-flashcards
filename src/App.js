af6dc203ba0c4fb05c36a11d6ef2edc5fc6eeefd
julianleahy <julianleahynz@gmail.com>
2018-03-26 16:01:47 +1300

    create ref to firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/database";
+import { DB_CONFIG } from "./Config/Firebase/db_config";
 
 class App extends Component {
+
+  // reference to firebase
+  app = firebase.initializeApp(DB_CONFIG);
+  database = this.app.database().ref().child('cards');
+
   render() {
     return (
       <div className="App">
