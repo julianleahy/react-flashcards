9b7b2674cbfad136427287d350142a2c000071d3
julianleahy <julianleahynz@gmail.com>
2018-03-26 17:18:44 +1300

    change btn text when finished

--- src/NewCard/NewCard.js
+++ src/NewCard/NewCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './NewCard.css'
 
-const NewCard = (props) => (
+const NewCard = (props) => {
+  const btnText = props.done ? 'Finished' : 'Next Question';
+  return (
     <button className='nextCard' onClick={props.next} disabled={props.done} >
-      Next Question
+      {btnText}
     </button>
-)
+  )
+}
 
 export default NewCard;
