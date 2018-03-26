28f7a4d2bcc7680ac8a1765858dbebce55bba44c
julianleahy <julianleahynz@gmail.com>
2018-03-26 16:33:20 +1300

    load data from fb and show card when all data is loaded

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import firebase from "firebase/app";
 import "firebase/database";
 import { DB_CONFIG } from "./Config/Firebase/db_config";
+import Card from "./Card/Card";
 
 class App extends Component {
 
@@ -10,10 +11,41 @@ class App extends Component {
   app = firebase.initializeApp(DB_CONFIG);
   database = this.app.database().ref().child('cards');
 
+  state = {
+    cards: [],
+    qNum: null,
+    loaded : false
+  }
+
+  componentWillMount() {
+    const currentCards = [];
+    // get last question number so we can tell when entire list is uploaded
+    this.database.limitToLast(1).once('child_added', snap => {
+      this.setState({ qNum: snap.val().q })
+      this.database.on('child_added', snap => {
+        currentCards.push({
+          id: snap.key,
+          question: snap.val().question,
+          answer: snap.val().answer,
+          asked: snap.val().asked
+        })
+        // update loaded only when all questions are loaded
+        if(currentCards.length === this.state.qNum){
+          this.setState({loaded : true, cards: currentCards})
+        }
+      })
+    })
+  }
+
   render() {
+    // wait for all data before displaying card
+    let card = 'Loading ...';
+    if(this.state.loaded) {
+      card = <Card />
+    }
     return (
       <div className="App">
-        
+        {card}
       </div>
     );
   }
