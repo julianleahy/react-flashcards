de5a6378fa48c4f967775809909ff53514b3b426
julianleahy <julianleahynz@gmail.com>
2018-03-26 16:47:18 +1300

    add newcard component to select next question

---
+++ src/NewCard/NewCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NewCard.css'
+
+const NewCard = (props) => (
+    <button className='nextCard'>
+      Next Question
+    </button>
+)
+
+export default NewCard;
